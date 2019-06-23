7af2662d23c5b2c1560251cb88e70534e00ea93a
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-06-23 08:52:31 +0300

    update i2c initialization

--- src/i2c.c
+++ src/i2c.c
@@ -2,8 +2,8 @@
 
 I2C_HandleTypeDef i2c2;
 
-void MX_I2C2_Init() {
-    //TODO slave init
+void MX_I2C2_Init()
+{
     i2c2.Instance             = I2C2;
     i2c2.Init.ClockSpeed      = 400000;
     i2c2.Init.DutyCycle       = I2C_DUTYCYCLE_2;
@@ -16,5 +16,44 @@ void MX_I2C2_Init() {
 
     if (HAL_I2C_Init(&i2c2) != HAL_OK) {
         Error_Handler();
+    }
+}
+
+void HAL_I2C_MspInit(I2C_HandleTypeDef* i2c)
+{
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    if (i2c->Instance == I2C2) {
+        /* GPIOA clock enable */
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+
+        /* I2C2 clock enable */
+        __HAL_RCC_I2C2_CLK_ENABLE();
+
+        /**
+         * I2C2 GPIO Configuration
+         * PB10 ------> I2C2_SCL
+         * PB11 ------> I2C2_SDA
+         */
+        GPIO_InitStruct.Pin   = GPIO_PIN_10|GPIO_PIN_11;
+        GPIO_InitStruct.Mode  = GPIO_MODE_AF_OD;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    }
+}
+
+void HAL_I2C_MspDeInit(I2C_HandleTypeDef* i2c)
+{
+    if (i2c->Instance == I2C2) {
+        /* I2C2 clock disable */
+        __HAL_RCC_I2C2_CLK_DISABLE();
+
+        /**
+         * I2C2 GPIO Configuration
+         * PB10 ------> I2C2_SCL
+         * PB11 ------> I2C2_SDA
+         */
+        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_10|GPIO_PIN_11);
     }
 }
